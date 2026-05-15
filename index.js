5722d4cd9755f1212af7846475c08da5d63983f9
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-05-15 10:46:59 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const a = [20, 30, 40, 50, 60]
+
+let max = a[0]
+let min = a[0]
+let total = 0
+
+for (let i = 0; i <= a.length; i++) {
+    if (a[i] > max) {
+        max = a[i]
+        if (a[i] < min) {
+            min = a[i]
+        }
+        total += a[i]
+    }
+}
+let avarage = total / a.length
+
+console.log(max)
+console.log(min)
+console.log(avarage)
