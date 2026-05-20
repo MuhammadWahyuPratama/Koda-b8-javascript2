4c9e224f910c68fd72f4f79199c2a74c1e345de8
Muhammadwahyu1212 <muhammad123wahyu123@gmail.com>
2026-05-20 14:06:50 +0700

    fix : add function main()

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ let max = a[0]
 let min = a[0]
 let total = 0
 
-for (let i = 0; i <= a.length; i++) {
-    if (a[i] > max) {
-        max = a[i]
-        if (a[i] < min) {
-            min = a[i]
+
+function main() {
+    for (let i = 0; i <= a.length; i++) {
+        if (a[i] > max) {
+            max = a[i]
+            if (a[i] < min) {
+                min = a[i]
+            }
+            total += a[i]
         }
-        total += a[i]
     }
+    console.log(max)
+    console.log(min)
+    console.log(total / a.length)
 }
 
-console.log(max)
-console.log(min)
-console.log(total / a.length)
+main()
+
+
